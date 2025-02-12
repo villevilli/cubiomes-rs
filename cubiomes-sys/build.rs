397152ebe0b302737e9e6a4abfc4e537f56846f5
Villsukka <kujala.ville.vk@gmail.com>
2025-02-13 00:02:07 +0200

    bindgen can generate and link the project now

---
+++ cubiomes-sys/build.rs
@@ -0,0 +1,55 @@
+use std::{collections::HashSet, env, path::PathBuf, process::Command};
+
+#[derive(Debug)]
+struct IgnoreMacros(HashSet<String>);
+
+impl bindgen::callbacks::ParseCallbacks for IgnoreMacros {
+    fn will_parse_macro(&self, name: &str) -> bindgen::callbacks::MacroParsingBehavior {
+        if self.0.contains(name) {
+            bindgen::callbacks::MacroParsingBehavior::Ignore
+        } else {
+            bindgen::callbacks::MacroParsingBehavior::Default
+        }
+    }
+}
+
+fn main() {
+    Command::new("make")
+        .arg("-C")
+        .arg("cubiomes/")
+        .spawn()
+        .expect("Failed to build cubiomes");
+
+    Command::new("mv")
+        .arg("cubiomes/libcubiomes.a")
+        .arg(env::var("OUT_DIR").unwrap())
+        .spawn()
+        .expect("Failed to move libcubiomes");
+
+    println!("cargo:rustc-link-search={}", env::var("OUT_DIR").unwrap());
+    println!("cargo:rustc-link-lib=static=cubiomes");
+
+    let ignored_macros = IgnoreMacros(
+        vec![
+            "FP_INFINITE".into(),
+            "FP_NAN".into(),
+            "FP_NORMAL".into(),
+            "FP_SUBNORMAL".into(),
+            "FP_ZERO".into(),
+        ]
+        .into_iter()
+        .collect(),
+    );
+
+    let bindings = bindgen::Builder::default()
+        .header("wrapper.h")
+        .parse_callbacks(Box::new(ignored_macros))
+        .generate()
+        .expect("Unable to generate binding for cubiomes");
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings")
+}
---
+++ cubiomes-sys/wrapper.h
@@ -0,0 +1,8 @@
+#include "cubiomes/biomenoise.h"
+#include "cubiomes/biomes.h"
+#include "cubiomes/finders.h"
+#include "cubiomes/generator.h"
+#include "cubiomes/layers.h"
+#include "cubiomes/noise.h"
+#include "cubiomes/quadbase.h"
+#include "cubiomes/util.h"
