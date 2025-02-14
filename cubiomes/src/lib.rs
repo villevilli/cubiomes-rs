56a416055204cf175df370ee800dd8d7d5caca21
Ville Kujala <kujala.ville.vk@gmail.com>
2025-02-14 17:22:17 +0200

    updates docs

--- cubiomes/src/lib.rs
+++ cubiomes/src/lib.rs
@@ -1,3 +1,10 @@
+//! A safe wrapper for the cubiomes library
+//!
+//! This crate provides safe bindings for [`cubiomes`]: https://github.com/Cubitect/cubiomes by cubitect.
+//!
+//! For biome generation see [`crate::generator`]
+//!
+
 #![warn(clippy::undocumented_unsafe_blocks)]
 
 pub mod generator;
