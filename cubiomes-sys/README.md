30b4dbf6a1f0cb91c490e3567ce88e25c5ff083c
Ville Kujala <kujala.ville.vk@gmail.com>
2025-02-14 13:28:56 +0200

    makes default build use rust cc crate

--- cubiomes-sys/README.md
+++ cubiomes-sys/README.md
@@ -3,3 +3,7 @@ Rust raw-ffi bindings for the cubiomes library made by cubitect
 Original library can be found in https://github.com/Cubitect/cubiomes
 
 Bindings generated with bindgen. The crate also statically links cubiomes
+
+## Cargo features
+``cc_build``, use the cc create for building, enabled by default. If not enabled
+cubiomes is built with make
--- cubiomes-sys/build.rs
+++ cubiomes-sys/build.rs
@@ -1,4 +1,26 @@
-use std::{collections::HashSet, env, path::PathBuf, process::Command};
+#[cfg(not(feature = "cc_build"))]
+use std::process::Command;
+use std::{collections::HashSet, env, path::PathBuf};
+
+macro_rules! add_prefix {
+    ($x:literal, $($y:literal),+) => {
+        [$(
+            concat!($x, $y),
+        )+]
+    };
+}
+
+const C_OBJECTS: [&'static str; 8] = add_prefix!(
+    "cubiomes/",
+    "noise.c",
+    "biomes.c",
+    "layers.c",
+    "biomenoise.c",
+    "generator.c",
+    "finders.c",
+    "util.c",
+    "quadbase.c"
+);
 
 #[derive(Debug)]
 struct IgnoreMacros(HashSet<String>);
@@ -23,25 +45,11 @@ impl bindgen::callbacks::ParseCallbacks for DeriveMacros {
 }
 
 fn main() {
-    if !Command::new("make")
-        .arg("-C")
-        .arg("cubiomes/")
-        .status()
-        .expect("Failed to build cubiomes")
-        .success()
-    {
-        panic!("Make did not return 0")
-    }
+    #[cfg(not(feature = "cc_build"))]
+    build_with_make();
 
-    if !Command::new("mv")
-        .arg("cubiomes/libcubiomes.a")
-        .arg(env::var("OUT_DIR").unwrap())
-        .status()
-        .expect("Failed to move libcubiomes")
-        .success()
-    {
-        panic!("mv did not return 0")
-    }
+    #[cfg(feature = "cc_build")]
+    build_with_cc();
 
     println!("cargo:rustc-link-search={}/", env::var("OUT_DIR").unwrap());
     println!("cargo:rustc-link-lib=static=cubiomes");
@@ -87,4 +95,32 @@ fn main() {
     biome_enum_bindings
         .write_to_file(out_path.join("biome_enums.rs"))
         .expect("Couldn't write biome enums");
+}
+
+#[cfg(not(feature = "cc_build"))]
+fn build_with_make() {
+    if !Command::new("make")
+        .arg("-C")
+        .arg("cubiomes/")
+        .status()
+        .expect("Failed to build cubiomes")
+        .success()
+    {
+        panic!("Make did not return 0")
+    }
+
+    if !Command::new("mv")
+        .arg("cubiomes/libcubiomes.a")
+        .arg(env::var("OUT_DIR").unwrap())
+        .status()
+        .expect("Failed to move libcubiomes")
+        .success()
+    {
+        panic!("mv did not return 0")
+    }
+}
+
+#[cfg(feature = "cc_build")]
+fn build_with_cc() {
+    cc::Build::new().files(C_OBJECTS).compile("cubiomes");
 }
