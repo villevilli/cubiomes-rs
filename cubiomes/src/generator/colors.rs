367c3e602b723133ec23b3bac8dcc481e5e4fb29
Ville Kujala <kujala.ville.vk@gmail.com>
2025-02-28 17:44:20 +0200

    adds documentation to BiomeColorMap

--- cubiomes/src/generator/colors.rs
+++ cubiomes/src/generator/colors.rs
@@ -1,4 +1,8 @@
-//! This module is used for biome color mappings
+//! This module is used for biome color mappings.
+//!
+//! The module contains a function to generate a [BTreeMap] of biome id:s to
+//! colors, and a faster [BiomeColorMap] which uses an array backend to map
+//! biomes into colors
 
 use crate::enums::BiomeID;
 use cubiomes_sys::num_traits::FromPrimitive;
@@ -6,7 +10,10 @@ use std::{collections::BTreeMap, mem::MaybeUninit, ops::Index};
 
 /// Function returns a map of biomeids to colors
 ///
-/// This function is useful for generating pictures of biome maps.
+/// This function is useful for generating pictures of biome maps as a binary tree.
+///
+/// Using a [`BiomeColorMap`] is probably faster, unless you desire a map type for
+/// some reason.
 ///
 /// The colors are mapped as an array, the elements corresponding
 /// to the Red Green And blue value respectively. eg \[RED, GREEN, BLUE]
@@ -33,7 +40,25 @@ pub fn new_biome_color_map() -> BTreeMap<BiomeID, [u8; 3]> {
         .collect()
 }
 
-#[derive(Debug)]
+/// A map of biomeids to colors
+///
+/// This function is useful for generating pictures of biome maps.
+///
+/// The colors are mapped as an array, the elements corresponding to the Red
+/// Green And blue value respectively. eg `[RED, GREEN, BLUE]` The map should
+/// contain all biomes available in minecraft.
+///
+/// The color scheme comes from cubiomes. The color scheme in cubiomes is
+/// strongly inspired by the color scheme used in
+/// [AMIDST](https://github.com/toolbox4minecraft/amidst/wiki/Biome-Color-Table)
+///
+/// It's implemented as its own type which uses an array as its backend. The
+/// array maps each biomeid number to a color. The id's which dont map to a id
+/// are 0.
+///
+/// This approach was benchmarked to be faster than a map type. If you still
+/// desire a seperate map type you can use [`new_biome_color_map()`] instead.
+#[derive(Debug, Clone, Copy)]
 pub struct BiomeColorMap([[u8; 3]; 256]);
 
 impl Default for BiomeColorMap {
@@ -43,6 +68,8 @@ impl Default for BiomeColorMap {
 }
 
 impl BiomeColorMap {
+    /// Constructs a new [BiomeColorMap] with colours from cubiomes.
+    #[must_use]
     pub fn new() -> Self {
         let mut colors: MaybeUninit<[[u8; 3]; 256]> = MaybeUninit::uninit();
 
@@ -55,11 +82,22 @@ impl BiomeColorMap {
         Self(unsafe { colors.assume_init() })
     }
 
+    /// Tries to get a specific [BiomeID]'s color from the map. If not found
+    /// (for some reason) returns [None]. The map should contain all biomes.
+    #[must_use]
+    pub fn get(&self, idx: BiomeID) -> Option<&[u8; 3]> {
+        self.0.get(idx as usize)
+    }
+
+    /// Returns a reference to the underlying array of the map.
+    #[must_use]
     #[inline]
     pub fn as_arr(&self) -> &[[u8; 3]; 256] {
         &self.0
     }
 
+    /// Returns a copy of the underlying array in the map.
+    #[must_use]
     #[inline]
     pub fn to_arr(&self) -> [[u8; 3]; 256] {
         self.0
